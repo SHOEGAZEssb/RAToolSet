76b31c23622c396932dd55b591e0a7e265d4ac18
Tim Stadler <coczero@users.noreply.github.com>
2015-01-19 23:24:09 +0000

    Removed everything php specific. Everything is now fetched via simple WebRequests. Added class for consoles. Achievement parsing now works. Achievement.cs + Game.cs property cleanup. Comments. Minor fixes

--- RAToolSet/RAToolSet/Achievement.cs
+++ RAToolSet/RAToolSet/Achievement.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RAToolSet
+﻿namespace RAToolSet
 {
+  /// <summary>
+  /// Represents a single achievement of a game.
+  /// </summary>
   class Achievement
   {
     int _id;
@@ -14,28 +11,147 @@ namespace RAToolSet
     string _title;
     string _description;
     int _points;
-    int _trueRatio; //??
-    string _author; //evtl User-Klasse?
+    int _trueRatio;
+    string _author;
     string _dateModified;
     string _dateCreated;
     int _badgeID;
     int _displayOrder;
+    string _memAddr;
+
+    /// <summary>
+    /// The id of this achievement.
+    /// </summary>
+    public int ID
+    {
+      get { return _id; }
+      private set { _id = value; }
+    }
+
+    /// <summary>
+    /// How often this achievement has been awarded.
+    /// </summary>
+    public int NumAwarded
+    {
+      get { return _numAwarded; }
+      private set { _numAwarded = value; }
+    }
+
+    /// <summary>
+    /// How often this achievement has been awarded on hardcore.
+    /// </summary>
+    public int NumAwardedHardcore
+    {
+      get { return _numAwardedHardcore; }
+      private set { _numAwardedHardcore = value; }
+    }
+
+    /// <summary>
+    /// The title of this achievement.
+    /// </summary>
+    public string Title
+    {
+      get { return _title; }
+      private set { _title = value; }
+    }
+
+    /// <summary>
+    /// The description of this achievement.
+    /// </summary>
+    public string Description
+    {
+      get { return _description; }
+      private set { _description = value; }
+    }
+
+    /// <summary>
+    /// The points this achievement is worth.
+    /// </summary>
+    public int Points
+    {
+      get { return _points; }
+      private set { _points = value; }
+    }
+
+    /// <summary>
+    /// The true ratio of this achievement.
+    /// </summary>
+    public int TrueRatio
+    {
+      get { return _trueRatio; }
+      private set { _trueRatio = value; }
+    }
+
+    /// <summary>
+    /// The name of the user who created this achievement.
+    /// </summary>
+    public string Author
+    {
+      get { return _author; }
+      private set { _author = value; }
+    }
+
+    /// <summary>
+    /// The date the achievement was modified last.
+    /// </summary>
+    public string DateModified
+    {
+      get { return _dateModified; }
+      private set { _dateModified = value; }
+    }
+
+    /// <summary>
+    /// The date the achievement was created.
+    /// </summary>
+    public string DateCreated
+    {
+      get { return _dateCreated; }
+      private set { _dateCreated = value; }
+    }
+
+    /// <summary>
+    /// The id of the badge of this achievement.
+    /// </summary>
+    public int BadgeID
+    {
+      get { return _badgeID; }
+      private set { _badgeID = value; }
+    }
+
+    /// <summary>
+    /// The display order of this achievement.
+    /// </summary>
+    public int DisplayOrder
+    {
+      get { return _displayOrder; }
+      private set { _displayOrder = value; }
+    }
+
+    /// <summary>
+    /// The memory addresses and their conditions for this achievement.
+    /// </summary>
+    public string MemAddr
+    {
+      get { return _memAddr; }
+      private set { _memAddr = value; }
+    }
 
     public Achievement(int id, int numAwarded, int numAwardedHardcore, string title, string description, int points, int trueRatio, string author,
-                       string dateModified, string dateCreated, int badgeID, int displayOrder)                       
+                       string dateModified, string dateCreated, int badgeID, int displayOrder, string memAddr)                       
     {
-      _id = id;
-      _numAwarded = numAwarded;
-      _numAwardedHardcore = numAwardedHardcore;
-      _title = title;
-      _description = description;
-      _points = points;
-      _trueRatio = trueRatio;
-      _author = author;
-      _dateModified = dateModified;
-      _dateCreated = dateCreated;
-      _badgeID = badgeID;
-      _displayOrder = displayOrder;
+      ID = id;
+      NumAwarded = numAwarded;
+      NumAwardedHardcore = numAwardedHardcore;
+      Title = title;
+      Description = description;
+      Points = points;
+      TrueRatio = trueRatio;
+      Author = author;
+      DateModified = dateModified;
+      DateCreated = dateCreated;
+      BadgeID = badgeID;
+      DisplayOrder = displayOrder;
+      MemAddr = memAddr;
     }
   }
 }
--- RAToolSet/RAToolSet/Game.cs
+++ RAToolSet/RAToolSet/Game.cs
@@ -1,6 +1,8 @@
-﻿
-namespace RAToolSet
+﻿namespace RAToolSet
 {
+  /// <summary>
+  /// Represents the basic information about a game.
+  /// </summary>
   class Game
   {
     private string _title;
@@ -9,43 +11,57 @@ namespace RAToolSet
     private string _consoleName;
     GameInfo _gameInfo;
 
+    /// <summary>
+    /// The title of this game.
+    /// </summary>
     public string Title
     {
-      get
-      {
-        return _title;
-      }
+      get { return _title; }
+      private set { _title = value; }
     }
 
+    /// <summary>
+    /// The id of this game.
+    /// </summary>
     public int ID
     {
-      get
-      {
-        return _id;
-      }
+      get { return _id; }
+      private set { _id = value; }
     }
 
+    /// <summary>
+    /// The id of the console this game is on.
+    /// </summary>
+    public int ConsoleID
+    {
+      get { return _consoleID; }
+      private set { _consoleID = value; }
+    }
+
+    /// <summary>
+    /// The name of the console this game is on.
+    /// </summary>
+    public string ConsoleName
+    {
+      get { return _consoleName; }
+      private set { _consoleName = value; }
+    }
+
+    /// <summary>
+    /// The extended information of this game.
+    /// </summary>
     public GameInfo GameInfo
     {
-      get
-      {
-        return _gameInfo;
-      }
-
-      set
-      {
-        _gameInfo = value;
-      }
+      get { return _gameInfo; }
+      set { _gameInfo = value; }
     }
 
     public Game(string title, int id, int consoleID, string consoleName)
     {
-      _title = title;
-      _id = id;
-      _consoleID = consoleID;
-      _consoleName = consoleName;
+      Title = title;
+      ID = id;
+      ConsoleID = consoleID;
+      ConsoleName = consoleName;
     }
-
-
   }
 }
