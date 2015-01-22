b9b46cb2ac771f2a25de293e2bd0c2d3f5cef75d
Timby <Timby@104ae323-1c64-a54c-b712-8bf454b4a520>
2015-01-22 20:54:58 +0000

    Added Form to display the rich presence script of a game. Web request times now get measured and printed in the status strip. Achievement Inspector now displays infos. Minor changes to visibilites.

---
+++ trunk/RAToolSet/RAToolSet/RichPresenceForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace RAToolSet
+{
+  partial class RichPresenceForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBoxRichPresence = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // textBoxRichPresence
+      // 
+      this.textBoxRichPresence.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.textBoxRichPresence.Location = new System.Drawing.Point(0, 0);
+      this.textBoxRichPresence.Multiline = true;
+      this.textBoxRichPresence.Name = "textBoxRichPresence";
+      this.textBoxRichPresence.ReadOnly = true;
+      this.textBoxRichPresence.Size = new System.Drawing.Size(638, 471);
+      this.textBoxRichPresence.TabIndex = 0;
+      // 
+      // RichPresenceForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(638, 471);
+      this.Controls.Add(this.textBoxRichPresence);
+      this.Name = "RichPresenceForm";
+      this.Text = "RichPresenceForm";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBoxRichPresence;
+  }
+}
---
+++ trunk/RAToolSet/RAToolSet/RichPresenceForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace RAToolSet
+{
+  /// <summary>
+  /// Displays the rich presence script of a game.
+  /// </summary>
+  public partial class RichPresenceForm : Form
+  {
+    /// <summary>
+    /// Ctor.
+    /// </summary>
+    /// <param name="richPresence">Rich presence script to display.</param>
+    public RichPresenceForm(string richPresence)
+    {
+      InitializeComponent();
+      textBoxRichPresence.Text = richPresence;
+    }
+  }
+}
